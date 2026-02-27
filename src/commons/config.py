a0d57e2a7ea7083155cde095943eae4dac3120ab
Sairam <dsairam1992@gmail.com>
2026-02-27 09:32:24 +0530

    samplesfffffg

--- src/commons/config.py
+++ src/commons/config.py
@@ -1,5 +1,7 @@
+import json
 import os
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 
 # load .env file
 # from pathlib import Path
@@ -11,16 +13,48 @@ from dotenv import load_dotenv
 # env_path = BASE_DIR / ".loca-variable-env"
 
 # load env file
-load_dotenv(".loca-variable-env")
+# load_dotenv(".loca-variable-env")
+
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+import boto3
+from botocore.exceptions import ClientError
 
 
-ENV = os.getenv("APP_ENV")
+def get_secret():
+
+    secret_name = "dev/mysql-details"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response["SecretString"]
+    return json.loads(secret)
+    # Your code goes here.
+
+
+result = get_secret()
+print(result)
+
+ENV = os.getenv("APP_ENV", "dev")
 if ENV == "dev":
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT", 3306)
-    schema = os.getenv("schema")
+    user = result.get("DB_NAME")
+    password = result.get("DB_PASSWORD")
+    host = result.get("DB_HOST")
+    port = result.get("DB_PORT", 3306)
+    schema = result.get("schema")
 
 
 BUCKET = "batch89-etl"
